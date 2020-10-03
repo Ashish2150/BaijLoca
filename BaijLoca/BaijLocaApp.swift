faf018e716adfb97728b34347f205f5e433a8c37
Ashish Kumar Maurya <ashishmauryait@gmail.com>
2020-10-03 23:57:43 +0530

    Initial Commit

---
+++ BaijLoca/BaijLocaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaijLocaApp.swift
+//  BaijLoca
+//
+//  Created by Ashish Kumar Maurya on 03/10/20.
+//
+
+import SwiftUI
+
+@main
+struct BaijLocaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
